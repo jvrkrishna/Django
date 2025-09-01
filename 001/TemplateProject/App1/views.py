c7882d999552eaff4c91f4c96d0290a2fc9edb83
jvrkrishna <jvrkrishnaa@gmail.com>
2025-09-01 11:14:38 +0530

    done

--- 001/TemplateProject/App1/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def addAdmission(request):
-    return render(request, 'App1/add-admission.html')
-
-def admissionReport(request):
-    return render(request, 'App1/admission-report.html')
